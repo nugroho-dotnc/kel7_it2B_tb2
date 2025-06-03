23644c39990df399c303248a084252ce2c51df8d
naufalbintang <naufalbintangpradana@gmail.com>
2025-06-03 12:36:21 +0700

    gallery

--- src/Resources/pages/gallery/index.jsx
+++ src/Resources/pages/gallery/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "../../../App.css";
+import "../../style/gallery.css";
+
 const Gallery = () => {
     return (
         <>
-        <h1>
-            Gallery
-        </h1>
+            <div className="h-screen bg-img1">
+                <div className="h-screen">
+                    <div className="mt-64 flex">
+                        <div className="h-screen bg-black-90 w-full"></div>
+                        <div className="h-screen"></div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
