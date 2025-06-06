49aefecc8d1be1ea02266a62c3d7f866fa76c403
naufalbintang <naufalbintangpradana@gmail.com>
2025-06-06 10:20:54 +0700

    layout page detail

--- src/Resources/pages/home/detail/index.jsx
+++ src/Resources/pages/home/detail/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import "../../../style/detail.css"
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 function MuseumDetail() {
   const { id } = useParams(); 
+      // eslint-disable-next-line no-unused-vars
       const [museum, setMuseum] = useState({})
       const fetchMuseum = async () =>{
           const response = await fetch("/data/museum.json")
@@ -14,10 +16,74 @@ function MuseumDetail() {
           fetchMuseum()
       }, [])
   return (
-    <div className='mt-96'>
-      <h2>{museum.nama}</h2>
-      <p>ID Produk: {id}</p>
-    </div>
+    <>
+      <div className="container-detail">
+        <div className="page1">
+          <div className="black-box">
+            <div className='deskripsi-museum'>
+              <p style={{fontWeight: "bold", fontSize: "48px"}}>Museum Nasional</p>
+              <p style={{fontSize: "20px"}}>Museum Nasional Republik Indonesia, atau Museum Gajah, adalah ikon di Jakarta dan museum terbesar di Asia Tenggara yang menyimpan lebih dari 190.000 artefak sejarah dan budaya bangsa. Dari peninggalan prasejarah hingga era kemerdekaan, koleksi arkeologi, etnografi, dan lainnya menawarkan perjalanan edukatif menelusuri kekayaan peradaban Indonesia.</p>
+              <a href="#page2" className='button'>
+                Lihat Koleksi
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="page2" id='page2'>
+          <div className="black-box">
+            <div className="page2-content">
+              <div className="judul">
+                <h1>Daftar Koleksi</h1>
+              </div>
+              <div className="frame-koleksi" style={{marginBottom: "24px"}}>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-koleksi">
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+                <div className='koleksi'>
+                  <div className='nama-koleksi'>
+                    <h1>Keris</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
